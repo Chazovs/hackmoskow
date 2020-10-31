bbac757659647fb7b8d4a2ecaf3a2dd2b33aae2b
Sergey Chazov <oitv18@gmail.com>
2020-10-31 12:42:55 +0300

    add teacher view

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::make('/mbou4260/lesson/start', 'POST', 'LessonController', 'start');
 Route::make('/mbou4260/lesson/get', 'GET', 'LessonController', 'start');
 Route::make('/mbou4260/lesson/get-js-test', 'GET', 'LessonController', 'getJsTest');
 Route::make('/mbou4260/lesson/test', 'GET', 'LessonController', 'test');
+Route::make('/mbou4260/lesson/teacher/get/work', 'GET', 'LessonController', 'getWork');
+
